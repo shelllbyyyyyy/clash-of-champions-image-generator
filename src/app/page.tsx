4222ff1a8b7b981a5a63e6cd3d5b1a0ec4fa9a04
Arif Ramdani <108125940+shelllbyyyyyy@users.noreply.github.com>
2024-07-13 13:36:37 +0700

    fix: responsive design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import FormClash from "@/components/section/forms";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between py-20 px-2">
       <FormClash />
     </main>
   );
